9806521e9bc365693a6cb4b8b1967aead17ce067
zzzonked <yaroslavkuts@gmail.com>
2020-02-27 12:14:20 +0200

    Updates

---
+++ precache-manifest.69273507f858b2b8fe493f0034b9ce5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f00e00edf72a89181d26a70114af40d",
+    "url": "/squares-builder-react-js/index.html"
+  },
+  {
+    "revision": "b0453d0e7b1216cc82c7",
+    "url": "/squares-builder-react-js/static/css/main.2ee84bc2.chunk.css"
+  },
+  {
+    "revision": "e6257bf6e68062ccd02d",
+    "url": "/squares-builder-react-js/static/js/2.4692c398.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/squares-builder-react-js/static/js/2.4692c398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0453d0e7b1216cc82c7",
+    "url": "/squares-builder-react-js/static/js/main.98b60298.chunk.js"
+  },
+  {
+    "revision": "e25f64e9c09a4e3ff0a1",
+    "url": "/squares-builder-react-js/static/js/runtime-main.5b292326.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/squares-builder-react-js/precache-manifest.69273507f858b2b8fe493f0034b9ce5d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/squares-builder-react-js/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
